1016a305f5b8d1adfe1c09ed92bd8934f49ad30d
Dani Lobera <lobera2001zgz@gmail.com>
2022-03-07 12:07:29 +0100

    footer, navbar, contact

--- blogclase/templatetags/blog_tags.py
+++ blogclase/templatetags/blog_tags.py
@@ -12,3 +12,31 @@ def categories_list(context):
         'request': context['request'],
         'categories': categories
     }
+
+@register.inclusion_tag('blog/components/tags_list.html',
+                        takes_context=True)
+def tags_list(context):
+    tags = Tag.objects.all()
+    return {
+        'request': context['request'],
+        'tags': tags
+    }
+
+    @register.inclusion_tag("blog/components/post_categories_list.html", takes_context=True)
+def post_categories_list(context):
+    page = context["page"]
+    post_categories = page.categories.all()
+    return {
+        "request": context["request"],
+        "post_categories": post_categories,
+    }
+
+
+@register.inclusion_tag("blog/components/post_tags_list.html", takes_context=True)
+def post_tags_list(context):
+    page = context["page"]
+    post_tags = page.tags.all()
+    return {
+        "request": context["request"],
+        "post_tags": post_tags,
+    }
